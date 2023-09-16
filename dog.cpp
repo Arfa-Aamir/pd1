7ca72ca91d47ba14b7eb214d1738fc0da8f851d8
Arfa-Aamir <144796293+Arfa-Aamir@users.noreply.github.com>
2023-09-16 06:22:30 -0700

    Add files via upload

---
+++ dog.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+main(){
+ cout<<"  o    ^____^             "<<endl;
+cout<<"     o  ( oo )\\________    "<<endl;
+cout<<endl;
+cout<<"        (____)\\        )\\/\\ "<<endl;
+cout<<"              ||--------|       "<<endl;
+cout<<"              ||       ||        "<<endl;
+}
---
+++ war.cpp
@@ -0,0 +1,18 @@
+ #include<iostream>
+using namespace std;
+main(){
+system("color 02");
+cout<<"     888888  88888   888    88888    "<<endl;
+cout<<"     88        88   88 88   88  88    "<<endl;
+cout<<"      88888    88   88  88  88888      "<<endl;
+cout<<"         88    88   888888  88   88     "<<endl;
+cout<<"     888888    88   88  88  88   88     "<<endl;
+cout<<""<<endl;
+cout<<""<<endl;
+cout<<""<<endl;
+cout<<"     88  88  88   888   8888    88888     "<<endl;
+cout<<"     88  88  88  88 88  88 88   88        "<<endl;
+cout<<"     88 8888 88  88  88 88888     888      "<<endl;
+cout<<"      888   888  888888 88   88    88       "<<endl;
+cout<<"       88   88   88  88 88     888888        "<<endl;
+}
